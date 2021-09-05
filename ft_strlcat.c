865e228d49cc80bb3ce37172e228f21f8dd019db
root <root@LAPTOP-SNDR-9750.localdomain>
2021-09-05 09:35:58 +0200

    part 1 finished

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,37 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/02 12:03:12 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/02 14:55:38 by slathouw         ###   ########.fr       */
+/*   Created: 2021/09/05 09:20:26 by slathouw          #+#    #+#             */
+/*   Updated: 2021/09/05 09:20:26 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcat (char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	n_bytes;
-	size_t	slen;
-	size_t	dlen;
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	cpy_len;
 
-	slen = ft_strlen(src);
-	dlen = ft_strlen(dst);
-	n_bytes = dstsize;
-	while (*dst)
-	{
-		dst++;
-		if (n_bytes > 0)
-			n_bytes--;
-	}
-	while (*src && n_bytes > 1)
-	{
-		*dst++ = *src++;
-		n_bytes--;
-	}
-	*dst = '\0';
-	if (dstsize > dlen)
-		return (dlen + slen);
-	return (dstsize + slen);
+	if (!dst)
+		return (0);
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len >= size)
+		return (size + src_len);
+	dst += dst_len;
+	size -= dst_len;
+	if (src_len >= size)
+		cpy_len = size - 1;
+	else
+		cpy_len = src_len;
+	ft_memcpy(dst, src, cpy_len);
+	*(dst + cpy_len) = 0;
+	return (dst_len + src_len);
 }
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 11:23:47 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/02 14:51:53 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/05 09:33:38 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,26 +33,17 @@ void	ft_bzero(void *s, size_t n);								//TODO:TEST
 void	*ft_calloc(size_t count, size_t size);						//TODO:TEST
 char	*ft_strdup(const char *s1);									//TODO:TEST
 void	*ft_memcpy(void *dst, const void *src, size_t n);			//TODO:TEST
-void	*ft_memmove(void *dst, const void *src, size_t len);		//TODO :TEST
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); 	//TODO :TEST
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize); 	//TODO :TEST
+void	*ft_memmove(void *dst, const void *src, size_t len);		//TODO:TEST
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize); 	//TODO:TEST
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize); 	//TODO:TEST
 void	*ft_memchr(const void *s, int c, size_t n); 				//TODO: TEST
 int		ft_memcmp(const void *s1, const void *s2, size_t n); 		//TODO: TEST
-
-// ----------------PART 2
-//TODO: ft_substr
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-//TODO: ft_strjoin
-char *ft_strjoin(char const *s1, char const *s2);
-//TODO: ft_strtrim
-char *ft_strtrim(char const *s1, char const *set);
-//TODO: ft_split
-char **ft_split(char const *s, char c);
-//TODO: ft_itoa
-char 	*ft_itoa(int n);
-//TODO: ft_strmapi
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-//TODO: ft_striteri
-void ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_substr(char const *s, unsigned int start, size_t len);  //TODO: TEST
+char *ft_strjoin(char const *s1, char const *s2);                   //TODO: TEST
+char *ft_strtrim(char const *s1, char const *set);                  //TODO: TEST
+char **ft_split(char const *s, char c);                             //TODO: TEST
+char 	*ft_itoa(int n);                                            //TODO: TEST
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));     //TODO: TEST
+void ft_striteri(char *s, void (*f)(unsigned int, char*));          //TODO: TEST
 
 #endif
