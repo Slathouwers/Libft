a25c01e81ec743464721d5d48055c1546151c4e1
root <root@LAPTOP-SNDR-9750.localdomain>
2021-09-06 15:21:20 +0200

    fixed NULL case segfault

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 11:47:25 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/05 12:00:43 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/06 15:05:16 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@ void    ft_lstadd_back(t_list **lst, t_list *new)
     {
         if(*lst)
         {
-             tmp = ft_lstlast(*lst);
-             if (tmp)
-                tmp->next = new;
-            else
-                *lst = new;
+            tmp = ft_lstlast(*lst);
+            tmp->next = new;
         }
+        else
+            *lst = new;
     }
 }
