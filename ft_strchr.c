342b686624c4027dfe96ffd936b455b442939537
root <root@LAPTOP-SNDR-9750.localdomain>
2021-09-06 10:48:23 +0200

    fixed for int c > 256

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slathouw <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 13:30:08 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/02 14:46:26 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/06 10:35:39 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != c)
+	char	chr;
+
+	chr = c % 256;
+	while (*s != chr)
 	{
 		if (!*s)
 			return (NULL);
