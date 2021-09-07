ed96dd8b4c8f72e342f1686a0eae579d4ff2ebf1
Sander Lathouwers <slathouw@fu-r6-p4.s19.be>
2021-09-07 13:10:52 +0200

    type cast added

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 13:39:22 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/07 10:21:21 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/07 11:36:06 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	const char	*end;
+	char chr;
 
+	chr = (char) c;
 	if (!s)
 		return (NULL);
 	end = s;
 	while (*end)
 		end++;
-	if (c == '\0')
+	if (chr == '\0')
 		return ((char *) end);
 	while (--end >= s)
-		if (c == *end)
+		if (chr == *end)
 			return ((char *) end);
 	return (NULL);
 }
