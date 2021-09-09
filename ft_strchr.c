99e1c0d487dfebeb33a81e501d5e644e595e5c2f
Sander Lathouwers <slathouw@student.s19.be>
2021-09-09 09:14:51 +0200

    protected against NULL-ptr dereferencing

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 13:30:08 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/07 11:34:46 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/09 08:24:26 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strchr(const char *s, int c)
 {
 	char	chr;
 
+	if (!s)
+		return (NULL);
 	chr = (char) c;
 	while (*s != chr)
 	{
