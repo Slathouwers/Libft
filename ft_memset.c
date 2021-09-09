53477c62cd3cdb5c14f973dc19a7b400ca2007ae
Sander Lathouwers <slathouw@student.s19.be>
2021-09-09 09:13:56 +0200

    protected against NULL-ptr dereferencing

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 15:45:25 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/06 15:55:58 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/09 08:52:51 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*dst;
 	unsigned char	val;
 
+	if (!b)
+		return (NULL);
 	dst = (unsigned char *) b;
 	val = (unsigned char) c;
 	while (len-- > 0)
