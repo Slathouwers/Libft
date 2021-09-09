4985cc7b71b53d601edd2e4dcf5df11e0d6d7b7e
Sander Lathouwers <slathouw@student.s19.be>
2021-09-09 09:39:39 +0200

    fixed return when given NULL-ptr or 0 size

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: slathouw <slathouw@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 11:29:58 by slathouw          #+#    #+#             */
-/*   Updated: 2021/09/09 09:13:01 by slathouw         ###   ########.fr       */
+/*   Updated: 2021/09/09 09:34:27 by slathouw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	srclen;
 
-	if (!dst || !src || !dstsize)
-		return (0);
 	srclen = ft_strlen(src);
+	if (!dst || !src || !dstsize)
+		return (srclen);
 	if (srclen + 1 < dstsize)
 		ft_memmove(dst, src, srclen + 1);
 	else if (dstsize)
